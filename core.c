54fc05ec2d9ed6481e3b7b5a8d373915ca824bd2
maripasa <main.grudge573@aleeas.com>
2023-08-17 21:15:55 -0300

    Added colision, organized code and fixed movement

--- core.c
+++ core.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 
+// Forward declaration of the struct mobSettings
+struct mobSettings;
+
 // Function declarations
 void clearScreen();
 void showScreen(char matrix[11][11]);
@@ -10,8 +13,10 @@ void buildMap(char matrix[11][11]);
 void introduction();
 void bootIntroduction();
 char takeInput();
+void handlePlayerMovement(struct mobSettings *player, char direction, char matrix[11][11]);
+int availableSpace(struct mobSettings *player, char matrix[11][11]);
 
-// Global Structs
+// Global Struct
 struct mobSettings {
     int playerY;
     int playerX;
@@ -21,38 +26,39 @@ struct mobSettings {
 int main() {
     clearScreen();
 
+    // Initialize player settings
     struct mobSettings player;
     player.playerX = 5;
     player.playerY = 5;
     player.playerSkin = 'O';
 
-    char charInput[100];
-    int intInput = 0;
-
+    // Display boot introduction and wait for input
     bootIntroduction();
 
-    printf("      ⚙  <Enter any input> ⚙\n");
-    sleep(1);
-    printf("> ");
-    scanf("%s", charInput);
-
-    while (1){
+    while (1) {
         sleep(1);
         char matrix[11][11];
         buildMap(matrix);
         matrix[player.playerY][player.playerX] = player.playerSkin;
         showScreen(matrix);
-        if (strcmp(takeInput(), "w") != 0) {
-        }else if {
-    
+
+        char direction = takeInput();
+
+        // Check for user input and perform actions
+        if (direction == 'w' || direction == 'a' || direction == 's' || direction == 'd') {
+            handlePlayerMovement(&player, direction, matrix);
+        } else {
+            // Handle other input cases
+        }
+    }
 
     return 0;
 }
 
 void bootIntroduction() {
-    printf("Está pronto? [S/N]"); 
+    printf("Está pronto? [S/N]\n");
     sleep(1);
-    printf("Dica: <skip> para pular a introdução.\"\n");
+    printf("\"Dica: <skip> para pular a introdução.\"\n");
     sleep(1);
 
     char charInput[100];
@@ -63,29 +69,30 @@ void bootIntroduction() {
 }
 
 void buildMap(char matrix[11][11]) {
-    char initialMatrix[11][11] = { 
-    {'+', '=', '=', '=', '=', '=', '=', '=', '=', '=', '+'},
-    {'|', '~', '~', '~', '~', '~', '~', '~', '~', '~', '|'},
-    {'|', '~', '~', '~', '~', '~', '~', '~', '~', '~', '|'},
-    {'|', '~', '~', '~', '~', '~', '~', '~', '~', '~', '|'},
-    {'|', '~', '~', '~', '~', '~', '~', '~', '~', '~', '|'},
-    {'|', '~', '~', '~', '~', '~', '~', '~', '~', '~', '|'},
-    {'|', '~', '~', '~', '~', '~', '~', '~', '~', '~', '|'},
-    {'|', '~', '~', '~', '~', '~', '~', '~', '~', '~', '|'},
-    {'|', '~', '~', '~', '~', '~', '~', '~', '~', '~', '|'},
-    {'|', '~', '~', '~', '~', '~', '~', '~', '~', '~', '|'},
-    {'+', '=', '=', '=', '=', '=', '=', '=', '=', '=', '+'}
+    clearScreen();
+    char initialMatrix[11][11] = {
+        {'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'},
+        {'W', '~', '~', '~', '~', '~', '~', '~', '~', '~', 'W'},
+        {'W', '~', '~', '~', '~', '~', '~', '~', '~', '~', 'W'},
+        {'W', '~', '~', '~', '~', '~', '~', '~', '~', '~', 'W'},
+        {'W', '~', '~', 'W', '~', '~', '~', '~', '~', '~', 'W'},
+        {'W', '~', '~', '~', '~', '~', '~', 'W', '~', '~', 'W'},
+        {'W', '~', '~', '~', '~', '~', '~', '~', '~', '~', 'W'},
+        {'W', '~', 'I', '~', '~', '~', '~', '~', '~', '~', 'W'},
+        {'W', '~', '~', '~', '~', '~', '~', '~', '~', '~', 'W'},
+        {'W', '~', '~', '~', '~', '~', '~', '~', '~', '~', 'W'},
+        {'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'}
+        };
 
-    };
-
+    // Copy initial map layout to the game matrix
     for (int i = 0; i < 11; i++) {
         for (int j = 0; j < 11; j++) {
-            matrix[i][j] = initialMatrix[i][j];  // Copy values from initialMatrix
+            matrix[i][j] = initialMatrix[i][j];
         }
     }
 }
 
-void introduction(){
+void introduction() {
     clearScreen();
     printf("No ir do tempo, desperta um autômato de mistério e potencial.\n");
     sleep(4);
@@ -99,21 +106,22 @@ void introduction(){
     sleep(3);
     printf("Antigas ruínas são abraçadas pela vegetação exuberante.\n\n");
     sleep(5);
-    printf("    ██████╗ ███████╗ █████╗ ██████╗     ███████╗██╗  ██╗██╗███████╗████████╗\n");
+    printf("    ██████╗ ███████╗ █████╗ ██████╗ ███████╗██╗  ██╗██╗███████╗████████╗\n");
     sleep(1);
-    printf("   ██╔════╝ ██╔════╝██╔══██╗██╔══██╗    ██╔════╝██║  ██║██║██╔════╝╚══██╔══╝\n");
+    printf("   ██╔════╝ ██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║██║██╔════╝╚══██╔══╝\n");
     sleep(1);
-    printf("   ██║  ███╗█████╗  ███████║██████╔╝    ███████╗███████║██║█████╗     ██║\n"); 
+    printf("   ██║  ███╗█████╗  ███████║██████╔╝███████╗███████║██║█████╗     ██║\n"); 
     sleep(1);
-    printf("   ██║   ██║██╔══╝  ██╔══██║██╔══██╗    ╚════██║██╔══██║██║██╔══╝     ██║\n"); 
+    printf("   ██║   ██║██╔══╝  ██╔══██║██╔══██╗╚════██║██╔══██║██║██╔══╝     ██║\n"); 
     sleep(1);
-    printf("   ╚██████╔╝███████╗██║  ██║██║  ██║    ███████║██║  ██║██║██║        ██║\n"); 
+    printf("   ╚██████╔╝███████╗██║  ██║██║  ██║███████║██║  ██║██║██║        ██║\n"); 
     sleep(1);
-    printf("    ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝ \n\n");
-    sleep(1);
-}
+    printf("    ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝ \n\n");
+    sleep(3);
+    }
 
 void showScreen(char matrix[11][11]) {
+    // Display each cell of the game matrix
     for (int i = 0; i < 11; i++) {
         for (int j = 0; j < 11; j++) {
             printf("%2c ", matrix[i][j]);
@@ -127,8 +135,36 @@ void clearScreen() {
 }
 
 char takeInput() {
-
     char charInput[100];
     scanf("%s", charInput);
-    return charInput
+    return charInput[0];
+}
+
+void handlePlayerMovement(struct mobSettings *player, char direction, char matrix[11][11]) {
+    // Move the player based on the given direction
+    switch (direction) {
+        case 'w':
+            if (player->playerY > 0 && matrix[player->playerY - 1][player->playerX] != 'W') {
+                player->playerY--;
+            }
+            break;
+        case 'a':
+            if (player->playerX > 0 && matrix[player->playerY][player->playerX - 1] != 'W') {
+                player->playerX--;
+            }
+            break;
+        case 's':
+            if (player->playerY < 10 && matrix[player->playerY + 1][player->playerX] != 'W') {
+                player->playerY++;
+            }
+            break;
+        case 'd':
+            if (player->playerX < 10 && matrix[player->playerY][player->playerX + 1] != 'W') {
+                player->playerX++;
+            }
+            break;
+        default:
+            // Handle invalid input
+            break;
+    }
 }
